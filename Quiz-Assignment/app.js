271357bd4ab4dc79b79dc04b5f1609d0bf18c4e5
Saurav Silwal <31697142+sauravsilwal@users.noreply.github.com>
2018-10-22 19:03:49 -0400

    Add files via upload

---
+++ Quiz-Assignment/app.js
@@ -0,0 +1,51 @@
+const quiz = [
+	{
+		question: "",
+		answers: [
+			{ src: "images/", score:  },
+			{ src: "images/", score:  },
+			{ src: "images/", score:  },
+			{ src: "images/", score:  }
+		] 
+	}, 
+	
+    {
+		question: "",
+		answers: [
+			{ src: "images/", score:  }, 
+			{ src: "images/", score:  }, 
+			{ src: "images/", score:  }, 
+			{ src: "images/", score:  }
+		]
+	},
+	
+    {
+		question: "", 
+		answers:[
+			{ src: "images/", score:  },
+			{ src: "images/", score:  },
+			{ src: "images/", score:  }
+		]
+	},
+     
+    {
+		question: "",
+		answers: [
+			{ src: "images/", score:  }, 
+			{ src: "images/", score:  }, 
+			{ src: "images/", score:  }, 
+			{ src: "images/", score:  }
+		]
+	}
+            
+];
+
+
+
+
+
+
+
+
+
+
